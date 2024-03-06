41df5d6b0d11145729e4ecd0567c02012a5bbcad
Auri Munoz <amunozhe@redhat.com>
2024-03-06 08:46:38 +0100

    first commit

---
+++ src/main/java/greeting/HelloService.java
@@ -0,0 +1,45 @@
+package greeting;
+
+import hello.Hello;
+import hello.HelloOuterClass;
+import io.quarkus.grpc.GrpcService;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+
+import java.time.Duration;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static hello.HelloOuterClass.*;
+
+@GrpcService
+public class HelloService implements Hello {
+    @Override
+    public Uni<HelloReply> sayHello(HelloRequest request) {
+        return Uni.createFrom().item(()->HelloReply.newBuilder().setMessage("Hello " + request.getName()).build());
+    }
+
+    @Override
+    public Multi<HelloReply> streamHello(HelloOuterClass.Void request) {
+        Random random = new Random();
+        int i = random.nextInt(22);
+        return Multi.createFrom().ticks().every(Duration.ofSeconds(1)).onItem().transform(s-> HelloReply.newBuilder().setMessage(randomString(i)).build());
+    }
+
+    public static String randomString(int longitud) {
+
+        String lettersCollection = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        String result = "";
+        for (int x = 0; x < longitud; x++) {
+            int randomIndex = randomNumberBetween(0, lettersCollection.length() - 1);
+            char randomChar = lettersCollection.charAt(randomIndex);
+            result += randomChar;
+        }
+        return result;
+    }
+
+    public static int randomNumberBetween(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+
+}
---
+++ src/test/java/greeting/HelloServiceTest.java
@@ -0,0 +1,26 @@
+package greeting;
+
+import hello.Hello;
+import hello.HelloOuterClass;
+import io.quarkus.grpc.GrpcClient;
+import io.quarkus.test.junit.QuarkusTest;
+import io.smallrye.mutiny.Uni;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+@QuarkusTest
+public class HelloServiceTest {
+
+    @GrpcClient
+    Hello helloService;
+
+    @Test
+    public void shouldSayHello(){
+        Uni<HelloOuterClass.HelloReply> from_tests = helloService.sayHello(HelloOuterClass.HelloRequest.newBuilder().setName("from Tests").build());
+
+        HelloOuterClass.HelloReply helloReply = from_tests.await().atMost(Duration.ofSeconds(5));
+        Assertions.assertEquals(helloReply.getMessage(), "Hello from Tests");
+    }
+}
